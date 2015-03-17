100d62fc06106c14af60e52bac0ebf4d9ae54d2f
mops1k <bednyj.mops@gmail.com>
2015-03-17 17:14:49 +0300

    added public profile page

--- src/Blog/ServiceBundle/Controller/MainController.php
+++ src/Blog/ServiceBundle/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace Blog\ServiceBundle\Controller;
 
+use Doctrine\ORM\AbstractQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -10,20 +11,27 @@ class MainController extends Controller
 {
     public function indexAction(Request $request)
     {
-//        $page = $this->getDoctrine()
-//            ->getManager()
-//            ->getRepository('BlogServiceBundle:Page')
-//            ->findOneBy([
-//                'name' => 'index'
-//            ])
-//        ;
-//
-//        if(!isset($page))
-//            throw new NotFoundHttpException();
         $result = null;
 
         if($request->getMethod() == 'POST') {
-            //
+            $em = $this->getDoctrine()
+                ->getEntityManager();
+            $qb = $em->createQueryBuilder();
+
+            $query = $qb->select('u,b')
+                ->from('BlogServiceBundle:User','u')
+                ->leftJoin('u.posts','b')
+                ->where('u.username LIKE :search')
+                ->andWhere('b.id is not null')
+                ->setParameter(':search','%'.$request->get('search').'%')
+            ;
+
+            $paginator  = $this->get('knp_paginator');
+            $result = $paginator->paginate(
+                $query,
+                $request->query->get('page', 1)/*page number*/,
+                10/*limit per page*/
+            );
         }
 
         return $this->render('BlogServiceBundle:Main:Index.html.twig',[
@@ -48,6 +56,23 @@ class MainController extends Controller
         return $this->render('BlogServiceBundle:Main:Page.html.twig',[
             'title' => $page->getTitle(),
             'page'  => $page
+        ]);
+    }
+
+    public function profileAction($name) {
+        $user = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('BlogServiceBundle:User')
+            ->findOneBy([
+                'username' => $name
+            ])
+        ;
+
+        if(!$user)
+            throw new NotFoundHttpException('User not found');
+
+        return $this->render('BlogServiceBundle:Main:Profile.html.twig',[
+            'user' => $user
         ]);
     }
 }
